4b9466f413a41d059503c52e8f4fd6de2c06d640
Izhan Sastre Hernando <izhansastre@gmail.com>
2024-11-03 20:48:48 +0100

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,69 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 12:34:42 by isastre-          #+#    #+#             */
-/*   Updated: 2024/11/03 19:12:22 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/11/03 20:45:18 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#define BUFFER_SIZE 12
+#define BUFFER_SIZE 10
+
+int	ft_read_line(int fd, char *line);
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	ssize_t	len;
+	static char	*line;
+	
+
+	if (!line)
+		line = "";
+	
+	if (ft_read_line(fd, line) == 0) // TODO free line + return NULL // add read error to same check?
+		return (NULL);
+	
+	return (line);
+}
+
+
+int	ft_read_line(int fd, char **line)
+{
+	char		*buffer;
+	ssize_t		len;
 
 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	len = read(fd, buffer, BUFFER_SIZE);
-	if (len == 0)
+	len = read(fd, buffer, BUFFER_SIZE); // TODO check read error (returns -1)
+	if (len == 0) // has reached EOF
 	{
-		free(buffer);
-		return (NULL);
+		free(buffer); // TODO where do I free line
+		return (0);
 	}
 	
 	buffer[BUFFER_SIZE] = '\0';
-	
-	return (buffer);
+
+	*line = ft_strjoin(*line, buffer);
+	if (ft_strchr(buffer, '\0') < BUFFER_SIZE)
+	{
+		printf("buffer: %s\n", buffer);
+		printf("line: %s\n", *line);
+		ft_read_line(fd, *line);
+	}
+	printf("lline: %s\n", *line);
+	return (len);
+}
+
+int	ft_strchr(const char *s, char ch)
+{
+	int	i;
+
+	i = 0;
+	if (ch == '\0')
+		return ft_strlen(s);
+	while (s[i])
+	{
+		if (s[i] == ch)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,20 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 12:34:30 by isastre-          #+#    #+#             */
-/*   Updated: 2024/11/03 18:47:22 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/11/03 20:17:15 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <stdio.h> // TODO borrar
+
 char	*get_next_line(int fd);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int	ft_strchr(const char *s, char ch);
+
+
