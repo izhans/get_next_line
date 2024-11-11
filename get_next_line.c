6ea7be603622e565b81bd4886f0c4a585b71a637
Izhan Sastre Hernando <izhansastre@gmail.com>
2024-11-11 13:54:25 +0100

    sometimes is better to start over again

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 12:34:42 by isastre-          #+#    #+#             */
-/*   Updated: 2024/11/03 19:12:22 by isastre-         ###   ########.fr       */
+/*   Updated: 2024/11/11 13:49:17 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,6 @@
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	ssize_t	len;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	len = read(fd, buffer, BUFFER_SIZE);
-	if (len == 0)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	
-	buffer[BUFFER_SIZE] = '\0';
-	
-	return (buffer);
+	return (NULL);
 }
